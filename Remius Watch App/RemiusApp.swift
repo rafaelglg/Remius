5994f8cb31c1928e8850948c00025ce6d44fa399
Rafael Loggiodice <rafael.loggiodice@edreamsodigeo.com>
2026-01-02 21:03:34 +0100

    Initial Commit

---
+++ Remius Watch App/RemiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemiusApp.swift
+//  Remius Watch App
+//
+//  Created by rafael.loggiodice on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Remius_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
